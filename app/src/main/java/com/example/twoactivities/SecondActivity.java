43773eeff3e4ab9bc92ef6414cefac7efd27c73c
DELL <DELL@DESKTOP-706JI0M>
2021-02-24 17:41:37 +0545

    edit text and button added for task4

--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -17,5 +18,8 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+    }
+
+    public void returnReply(View view) {
     }
 }
